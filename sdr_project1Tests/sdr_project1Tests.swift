de5cee945f07babee6f3048d9666bb91b1effb6c
심다래 <sdr2131@dongwon-gwak-ui-macbook-air.local>
2015-12-07 23:57:04 +0900

    Initial Commit

---
+++ sdr_project1Tests/sdr_project1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  sdr_project1Tests.swift
+//  sdr_project1Tests
+//
+//  Created by 심다래 on 2015. 12. 7..
+//  Copyright © 2015년 XNRND. All rights reserved.
+//
+
+import XCTest
+@testable import sdr_project1
+
+class sdr_project1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
